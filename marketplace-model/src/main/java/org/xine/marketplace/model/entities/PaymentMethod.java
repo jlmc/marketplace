ce9307e51d717f40c0c2ca08ee075a7d10345188
jlmc <costajlmpp@gmail.com>
2015-03-23 18:34:23 +0000

    util model enums

---
+++ marketplace-model/src/main/java/org/xine/marketplace/model/entities/PaymentMethod.java
@@ -0,0 +1,22 @@
+package org.xine.marketplace.model.entities;
+
+/**
+ * The Enum PaymentMethod.
+ */
+public enum PaymentMethod {
+	
+	/** The cash. */
+	CASH, 
+	
+	/** The credit card. */
+	CREDIT_CARD, 
+	
+	/** The debit card. */
+	DEBIT_CARD,
+	
+	/** The cheque. */
+	CHEQUE, 
+	
+	/** The deposit bank. */
+	DEPOSIT_BANK
+}
