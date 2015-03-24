be4c0f04838137e5b0ee6de89afd586444e4977f
JLC <JLC@TITAN.home>
2015-03-24 00:19:41 +0000

    add some missing doc

--- marketplace-repository/src/main/java/org/xine/marketplace/repository/util/Transactional.java
+++ marketplace-repository/src/main/java/org/xine/marketplace/repository/util/Transactional.java
@@ -7,6 +7,11 @@ import java.lang.annotation.Target;
 
 import javax.interceptor.InterceptorBinding;
 
+/**
+ * The Interface Transactional.
+ * existe um inteceptador desta anotação, das chamadas dos metodos anotados com esta anotação.
+ * {@code TransactionInterceptor#invoke(javax.interceptor.InvocationContext)}
+ */
 @InterceptorBinding
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD })
