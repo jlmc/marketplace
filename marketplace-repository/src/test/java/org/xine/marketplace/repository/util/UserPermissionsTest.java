a7e3b48ff2701edb4540268630412bb7cca18c27
jlmc <costajlmpp@gmail.com>
2015-03-24 18:32:54 +0000

    integration test helper

--- marketplace-repository/src/test/java/org/xine/marketplace/repository/util/UserPermissionsTest.java
+++ marketplace-repository/src/test/java/org/xine/marketplace/repository/util/UserPermissionsTest.java
@@ -1,33 +1,53 @@
 package org.xine.marketplace.repository.util;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import org.dbunit.dataset.common.handlers.Helper;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.xine.marketplace.model.entities.Permission;
 import org.xine.marketplace.model.entities.User;
 
+import com.jintegrity.core.JIntegrity;
+import com.jintegrity.helper.JPAHelper;
+
 public class UserPermissionsTest {
 
 	private EntityManager em;
+	
+	//private JIntegrity helper = new JIntegrity();
+	
+	private IntegrationHelper helper = new IntegrationHelper("marketplace-repository");
 
 	@Before
-	public void setUp() throws Exception {
-		em = JPAUtil.createEntityManager();
+	public void init() throws Exception {
+		//helper = new JIntegrity().path("xml").xml("User").insert();
+		//em = JPAUtil.createEntityManager();
+		
+		//helper.cleanAndInsert();
+		//em = JPAHelper.currentEntityManager();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		em.close();
+		//em.close();
+		//helper.clean();
+		this.helper.closeEntityManager();
 	}
 
 	@Test
+	@Ignore
 	public void createUserWithPermissions() {
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
+		em = JPAHelper.currentEntityManager();
+		
+		//EntityTransaction tx = em.getTransaction();
+		//tx.begin();
 		// //
 
 		User user = new User();
@@ -49,22 +69,35 @@ public class UserPermissionsTest {
 		em.persist(user);
 
 		// //////
-		tx.commit();
+		//tx.commit();
+	}
+	
+	@Test
+	public void getUser(){
+		
+		this.helper.init();
+		this.helper.cleanDB();
+		
+		em = this.helper.currentEntityManager();
+		
+		List<User> users = em.createQuery("from User").getResultList();
+		
+		//User user = em.find(User.class, Long.valueOf(1010));
+		//System.out.println(user.getUsername() + " - "+ user.getId());
+		Assert.assertNotNull(users);
+		
+		
 	}
 
 	@Test
 	@Ignore
 	public void createUser() {
-		EntityTransaction tx = em.getTransaction();
+		EntityTransaction tx = JPAUtil.createEntityManager().getTransaction();
 		tx.begin();
 		// //
 
-		User user = new User();
-		user.setPassword("passw");
-		user.setUsername("username-teste");
-		user.setEmail("TESTE-USER-1@EMAL");
-
-		em.persist(user);
+		
+		//em.persist(user);
 
 		// //////
 		tx.commit();
