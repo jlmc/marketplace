86efa03c8ba84cb77bbaacefd5a419550fdb811f
JLC <JLC@TITAN.home>
2015-04-01 03:46:06 +0100

    add test to the repository module project

--- marketplace-repository/src/main/java/org/xine/marketplace/repository/util/JPAUtil.java
+++ marketplace-repository/src/main/java/org/xine/marketplace/repository/util/JPAUtil.java
@@ -1,6 +1,5 @@
 package org.xine.marketplace.repository.util;
 
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -9,19 +8,21 @@ import javax.persistence.Persistence;
  * The Class JPAUtil.
  */
 public final class JPAUtil implements Constants {
-	/**
-	* Instantiates a new JPA util.
-	*/
-	private JPAUtil() {}
-	
-	/** The emf. */
-	private static EntityManagerFactory emf = Persistence
-	.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-	/**
-	* Creates the entity manager.
-	* @return the entity manager
-	*/
-	public static EntityManager createEntityManager() {
-	return emf.createEntityManager();
-	}
+    /**
+     * Instantiates a new JPA util.
+     */
+    private JPAUtil() {
+    }
+
+    /** The emf. */
+    private static EntityManagerFactory emf = Persistence
+            .createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+
+    /**
+     * Creates the entity manager.
+     * @return the entity manager
+     */
+    public static EntityManager createEntityManager() {
+        return emf.createEntityManager();
+    }
 }
---
+++ marketplace-repository/src/test/java/org/xine/marketplace/repository/util/GenerateDataBase.java
@@ -0,0 +1,15 @@
+package org.xine.marketplace.repository.util;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class GenerateDataBase {
+
+    @Test
+    @Ignore
+    public void testGenerate() {
+        JPAUtil.createEntityManager();
+
+    }
+
+}
