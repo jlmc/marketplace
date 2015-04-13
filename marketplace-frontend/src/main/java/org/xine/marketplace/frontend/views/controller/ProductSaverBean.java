8c67554eca0c4738dcf904211101e4087502b4c5
xine <xine@zeus.home>
2015-04-13 22:57:33 +0100

    fix bug

--- marketplace-frontend/src/main/java/org/xine/marketplace/frontend/views/controller/ProductSaverBean.java
+++ marketplace-frontend/src/main/java/org/xine/marketplace/frontend/views/controller/ProductSaverBean.java
@@ -8,35 +8,40 @@ import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+/**
+ * The Class ProductSaverBean.
+ */
 @Named
 @ViewScoped
 public class ProductSaverBean implements Serializable {
 
-    /**
-     *
-     */
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 1L;
 
+    /** The product. */
     private Product product;
 
+    /**
+     * Initialize.
+     */
     @PostConstruct
-    public void init() {
+    public void initialize() {
         this.product = new Product();
     }
 
-    // @Inject
-    // private ProductService productService;
-
+    /**
+     * Save.
+     */
     public void save() {
         System.out.println("save the product Action");
-
-        // if (this.productService != null) {
-        // this.productService.save(this.product);
-        // }
-
+        // TODO::missing implementation
         this.product = new Product();
     }
 
+    /**
+     * Gets the product.
+     * @return the product
+     */
     public Product getProduct() {
         return this.product;
     }
