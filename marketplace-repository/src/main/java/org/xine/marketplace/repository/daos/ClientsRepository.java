fe5c517b61780448ba23af51a4199a5e8de36c26
jlmc <costajlmpp@gmail.com>
2015-04-27 17:57:48 +0100

    clients Rep

--- marketplace-repository/src/main/java/org/xine/marketplace/repository/daos/ClientsRepository.java
+++ marketplace-repository/src/main/java/org/xine/marketplace/repository/daos/ClientsRepository.java
@@ -1,10 +1,7 @@
 package org.xine.marketplace.repository.daos;
 
-import org.xine.marketplace.model.entities.Address;
-import org.xine.marketplace.model.entities.Client;
-import org.xine.marketplace.model.filters.ClientFilter;
-
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -14,7 +11,12 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+
+import org.xine.marketplace.model.entities.Address;
+import org.xine.marketplace.model.entities.Client;
+import org.xine.marketplace.model.filters.ClientFilter;
 
 /**
  * The Class ClientsRepository.
@@ -30,16 +32,18 @@ public class ClientsRepository implements Serializable {
 
     /**
      * Gets the by id.
+     *
      * @param id
      *            the id
      * @return the by id
      */
     public Client getById(final Long id) {
-        return this.entityManager.find(Client.class, id);
+	return this.entityManager.find(Client.class, id);
     }
 
     /**
      * Gets the by id.
+     *
      * @param id
      *            the id
      * @param withAdreass
@@ -47,58 +51,75 @@ public class ClientsRepository implements Serializable {
      * @return the by id
      */
     public Client getById(final Long id, final boolean withAdreass) {
-        if (!withAdreass) {
-            return this.getById(id);
-        }
+	if (!withAdreass) {
+	    return this.getById(id);
+	}
 
-        final CriteriaBuilder criteriaBuilder = this.entityManager.getCriteriaBuilder();
-        final CriteriaQuery<Client> criteriaQuery = criteriaBuilder.createQuery(Client.class);
+	final CriteriaBuilder criteriaBuilder = this.entityManager
+		.getCriteriaBuilder();
+	final CriteriaQuery<Client> criteriaQuery = criteriaBuilder
+		.createQuery(Client.class);
 
-        final Root<Client> root = criteriaQuery.from(Client.class);
+	final Root<Client> root = criteriaQuery.from(Client.class);
 
-        // final Join<Client, Address> joiner = (Join) root.fetch("addresses");
-        // we must set JoinType.LEFT because of the clients that don't have addresses defined
-        @SuppressWarnings({"unchecked", "rawtypes", "unused" })
-        final Join<Client, Address> joiner = (Join) root.fetch("addresses", JoinType.LEFT);
+	// final Join<Client, Address> joiner = (Join) root.fetch("addresses");
+	// we must set JoinType.LEFT because of the clients that don't have
+	// addresses defined
+	@SuppressWarnings({ "unchecked", "rawtypes", "unused" })
+	final Join<Client, Address> joiner = (Join) root.fetch("addresses",
+		JoinType.LEFT);
 
-        criteriaQuery.select(root);
-        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), id));
+	criteriaQuery.select(root);
+	criteriaQuery.where(criteriaBuilder.equal(root.get("id"), id));
 
-        final TypedQuery<Client> query = this.entityManager.createQuery(criteriaQuery);
+	final TypedQuery<Client> query = this.entityManager
+		.createQuery(criteriaQuery);
 
-        // query.getSingleResult();
-        final Client c = Helper.getSingleResultUncheck(query);
+	// query.getSingleResult();
+	final Client c = Helper.getSingleResultUncheck(query);
 
-        return c;
+	return c;
     }
 
     /**
-     * Save.
+     * Save client.
+     *
      * @param client
      *            the client
      * @return the client
      */
-    public Client save(final Client client) {
-        return null;
+    public Client saveClient(final Client client) {
+	return this.entityManager.merge(client);
     }
 
     /**
      * Shearch.
+     *
      * @param filter
      *            the filter
      * @return the list
      */
     public List<Client> shearch(final ClientFilter filter) {
-        return null;
+
+	final CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+	final CriteriaQuery<Client> cQuery = builder.createQuery(Client.class);
+	final Root<Client> root = cQuery.from(Client.class);
+
+	cQuery.select(root);
+
+	final List<Predicate> predicates = new ArrayList<Predicate>();
+
+	return this.entityManager.createQuery(cQuery).getResultList();
     }
 
     /**
      * Sets the entity manager.
+     *
      * @param entityManager
      *            the new entity manager
      */
     public void setEntityManager(final EntityManager entityManager) {
-        this.entityManager = entityManager;
+	this.entityManager = entityManager;
     }
 
 }
---
+++ marketplace-repository/src/main/java/org/xine/marketplace/repository/daos/Strings.java
@@ -0,0 +1,19 @@
+package org.xine.marketplace.repository.daos;
+
+/**
+ * The Class Strings.
+ */
+final class Strings {
+
+    /**
+     * Checks if is null or blank.
+     *
+     * @param str
+     *            the str
+     * @return true, if is null or empty
+     */
+    public static boolean isNullOrBlank(final String str) {
+	return str == null || str.trim().length() == 0;
+    }
+
+}
