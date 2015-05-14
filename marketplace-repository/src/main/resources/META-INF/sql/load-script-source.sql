e4b10555c6305e88bca636b25fd97f5042bcd194
jlmc <costajlmpp@gmail.com>
2015-05-14 16:00:24 +0100

    add persistence 2.1 new properties

---
+++ marketplace-repository/src/main/resources/META-INF/sql/load-script-source.sql
@@ -0,0 +1,26 @@
+
+
+INSERT INTO category (id,description,masterCategory) VALUES (1,'Computers',NULL);
+INSERT INTO category (id,description,masterCategory) VALUES (2,'Gadgets',NULL);
+INSERT INTO category (id,description,masterCategory) VALUES (3,'Software',NULL);
+INSERT INTO category (id,description,masterCategory) VALUES (4,'Books', NULL);
+INSERT INTO category (id,description,masterCategory) VALUES (5,'Others',NULL);
+
+INSERT INTO category (id,description,masterCategory) VALUES (6,'Desktop',1);
+INSERT INTO category (id,description,masterCategory) VALUES (7,'Laptop',1);
+INSERT INTO category (id,description,masterCategory) VALUES (8,'Printer',1);
+INSERT INTO category (id,description,masterCategory) VALUES (9,'Tablet',1);
+
+INSERT INTO category (id,description,masterCategory) VALUES (10,'SO',3);
+INSERT INTO category (id,description,masterCategory) VALUES (11,'Office Application',3);
+INSERT INTO category (id,description,masterCategory) VALUES (12,'Driver',3);
+INSERT INTO category (id,description,masterCategory) VALUES (13,'House Application',3);
+
+
+INSERT INTO category (id,description,masterCategory) VALUES (14,'Computer sciencie',4);
+INSERT INTO category (id,description,masterCategory) VALUES (15,'Biographical',4);
+INSERT INTO category (id,description,masterCategory) VALUES (16,'Adventure',4);
+INSERT INTO category (id,description,masterCategory) VALUES (17,'Science fiction',4);
+INSERT INTO category (id,description,masterCategory) VALUES (18,'Romance',4);
+INSERT INTO category (id,description,masterCategory) VALUES (19,'Science fiction',4);
+INSERT INTO category (id,description,masterCategory) VALUES (20,'Police',4);
