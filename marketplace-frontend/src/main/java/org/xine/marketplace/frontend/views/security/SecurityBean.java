9024a3a38a7b22e305ab02521be35345d4618de4
jlmc <costajlmpp@gmail.com>
2015-05-20 09:55:21 +0100

    get Username of the session user

--- marketplace-frontend/src/main/java/org/xine/marketplace/frontend/views/security/SecurityBean.java
+++ marketplace-frontend/src/main/java/org/xine/marketplace/frontend/views/security/SecurityBean.java
@@ -1,7 +1,12 @@
 package org.xine.marketplace.frontend.views.security;
 
+import java.security.Principal;
+
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 /**
  * The Class SecurityBean.
@@ -16,9 +21,26 @@ public class SecurityBean {
      * Gets the username.
      * @return the username
      */
-    @SuppressWarnings("static-method")
     public String getUsername() {
-        return "Master-user";
+        String username = null;
+
+        final SystemUser systemUser = getSessionUser();
+        if (systemUser != null) {
+            username = systemUser.getUser().getUsername();
+        }
+        return username;
+    }
+
+    @SuppressWarnings("static-method")
+    private SystemUser getSessionUser() {
+        final Principal principal = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+        final UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) principal;
+
+        if (auth != null && auth.getPrincipal() != null) {
+            return (SystemUser) auth.getPrincipal();
+        }
+
+        return null;
     }
 
 }
