5b7eddc109fac536b7dbc615d3e7f9258b60856c
JLC <JLC@TITAN.home>
2015-06-02 01:01:49 +0100

    no test in report project because it is not used

--- marketplace-report/src/test/java/org/xine/marketplace/report/impl/RequisitionExecuterTest.java
+++ marketplace-report/src/test/java/org/xine/marketplace/report/impl/RequisitionExecuterTest.java
@@ -5,12 +5,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Locale;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.xine.marketplace.report.impl.utils.RequisitionReportItemFactory;
 
 public class RequisitionExecuterTest {
 
     @Test
+    @Ignore
     public void testGenerate() throws FileNotFoundException {
         final byte[] result = new RequisitionExecuter().generate(RequisitionReportItemFactory.createBeanCollection(), new Locale("pt", "PT"));
 
